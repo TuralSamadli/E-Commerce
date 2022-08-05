53ce9294db13dce65d5aa82d0e17570f766a3d78
Tural Samadli <tsamadli4696@ada.edu.az>
2022-08-05 15:06:31 +0400

    footer is done

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -4,4 +4,10 @@
             <div class="col-md-6">Eticaret &copy; 2022</div>
         </div>
     </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">Eticaret &copy; 2022</div>
+        </div>
+    </div>
+    <a href="https://www.facebook.com/"><p>Facebook </p></a>
 </footer>
